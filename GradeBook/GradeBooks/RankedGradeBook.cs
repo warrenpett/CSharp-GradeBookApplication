64f248300c9d3a42a8c9a8cae50a4b629f2fdca5
warrenpett <warrenpett1993@gmail.com>
2018-12-06 13:14:29 +0000

    Update RankedGradeBook.cs
    Create CalculateStatistics override methods

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -23,11 +23,11 @@ namespace GradeBook.GradeBooks
             var threshold = (int)Math.Ceiling(Students.Count * 0.2);
             var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
 
-            if(grades[threshold -1] <= averageGrade)
+            if(grades[threshold - 1] <= averageGrade)
             {
                 return 'A';
             }
-            else if(grades[(threshold * 2) -1] <= averageGrade)
+            else if(grades[(threshold * 2) - 1] <= averageGrade)
             {
                 return 'B';
             }
@@ -44,6 +44,30 @@ namespace GradeBook.GradeBooks
                 return 'F';
             }
 
+        }
+
+        public override void CalculateStatistics()
+        {
+            if(Students.Count <5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+            }
+            else
+            {
+                base.CalculateStatistics();
+            }
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+            }
+            else
+            {
+                base.CalculateStudentStatistics(name);
+            }
         }
     }
 }
